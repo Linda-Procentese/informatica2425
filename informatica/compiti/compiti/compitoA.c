cb7b0929bdf0a821d30f10bdf5f5af654e49a0e6
Linda-Procentese <linda.procentese@galileiostiglia.edu.it>
2024-09-30 19:24:41 +0000

    es

--- informatica/compiti/compiti/compitoA.c
+++ informatica/compiti/compiti/compitoA.c
@@ -7,11 +7,9 @@ int main ()
 {	float a=0;
     printf("inserisci il valore: ");
     scanf("%d",&a);
-    if (a/3)
-    {printf("il valore è divisibile per 3");
-    if(a/5)
-    {printf("il valore è divisibile sia per 3 sia per 5");
-    else 
-    printf("il valore è divisibile sia per 3 sia per 5");}
-    printf("il valore non è divisibile per 3");}
+    if ((a/3/5)!=0)
+    printf("il valore è divisibile sia per 3 sia per 5");
+    else
+    printf("il valore non è divisibile sia per 3 sia per 5");
+    
 }
