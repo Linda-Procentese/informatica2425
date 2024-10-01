207404a0b36865c45f813ec3bdb9ee0260dac496
Linda-Procentese <linda.procentese@galileiostiglia.edu.it>
2024-10-01 10:52:21 +0000

    esercizi

--- informatica/compiti/compiti/compitoA.c
+++ informatica/compiti/compiti/compitoA.c
@@ -4,12 +4,14 @@ Verificare se un numero dato in input è divisibile sia per 3 sia per 5
 #include <stdio.h>
 
 int main ()
-{	float a=0;
+{	int a=0;
     printf("inserisci il valore: ");
-    scanf("%f",&a);
-    if ((a%3==0 && a%5==0)
-    printf("il valore è divisibile sia per 3 sia per 5");
+    scanf("%d",&a);
+    if(a%3==0)
+        if(a%5==0)
+            printf("il valore è divisibile sia per 3 sia per 5");
+        else
+            printf("il valore non è divisibile per 3 ma non per 5");
     else
-    printf("il valore non è divisibile sia per 3 sia per 5");
-    
+        printf("il valore non è divisibile per 3");
 }
--- informatica/compiti/compiti/compitoB.c
+++ informatica/compiti/compiti/compitoB.c
@@ -3,16 +3,8 @@
 
 int main ()
 {
-    float a=0. b=0, c=0, bc=0, media=0;
-    printf("inserisci il 1' valore: ");
-    scanf("%d",&a);
-    printf("inserisci il 2' valore: ");
-    scanf("%d",&b);
-    printf("inserisci il 3' valore: ");
-    scanf("%d",&c);
-    if(a==b==c)
-    {printf("i tre numeri sono uguali");}
-    else
-    if (a<b&c)
-    printf("il numero più piccolo è: %f",a);
+    float a=0, b=0, c=0, bc=0, media=0;
+    printf("inserisci i 3 valori: ");
+    scanf("%d",&a,&b,&c);
+    
 }
