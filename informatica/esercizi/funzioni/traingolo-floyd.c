1c3215ac7a3d043390ef89999d902d25d8c6b708
Linda-Procentese <linda.procentese@galileiostiglia.edu.it>
2025-02-26 08:14:17 +0000

    es

--- informatica/esercizi/funzioni/traingolo-floyd.c
+++ informatica/esercizi/funzioni/traingolo-floyd.c
@@ -5,6 +5,7 @@ n=5
 4 5 6
 7 8 9 10
 11 12 13 14 15*/
+void triangolo(int _n);
 #include <stdio.h>
 int main(){
     int n;
@@ -12,8 +13,14 @@ int main(){
         printf("inserisci un numero: ");
         scanf("%d",&n);
     }while(n<=0);
-
-    for(int i=1;i<=n ;i++){
-        
-    }
+    triangolo
+    
 }
+void triangolo(int _n){
+    for(int riga=1;riga<=_n ;riga++){
+        for(int a=1;a<=riga;a++){
+            printf("%d\t",a);
+        }
+        printf("\n");
+    }
+}//non funziona
