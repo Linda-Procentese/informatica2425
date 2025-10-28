c6642d09f81d9fe3855a4d65268b99f45428dfa2
Linda-Procentese <linda.procentese@galileiostiglia.edu.it>
2025-10-28 09:58:52 +0000

    stringe

--- 4-superiore/informatica/stringhe/frasePalindroma.c
+++ 4-superiore/informatica/stringhe/frasePalindroma.c
@@ -1,64 +1,41 @@
-/*capire se una parola è palindroma
-es:
-anna è palindroma
-ciao non lo è*/
-
+/* Verificare se una stringa è palindroma 
+    Es: anna è palindroma
+       ciao non è palindroma */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef char* string;
-
-int lenght (string buffer){
-    int len=0;
-    while(buffer[len]!='\0')
-        len++;
-
-    return len;
-}
-
-void compatta(string buffer, int len){
-    if(len>0 && buffer[len-1]!='\n'){
-        buffer[len-1]='\0';
-        len--;
+typedef char* String;
+int lunghezza(String s){
+    int i=0;
+    while (s[i] != '\0'){
+        i++;
     }
+    return i-1;
 }
+int Palindroma(String s, int dim){
+    int flag=0, i, j;
 
-int palindroma(string frase,int len){
-    int flag=0,i,j;
-    for(int i=0;j=len-1;i<len/2 ;i++;j--){
-        if(frase[i]!=frase[j])
-            flag=1;//la frase è palindroma
+    for( i=0, j=dim-1; i<dim/2; i++, j--){
+        if(s[i]!=s[j])
+            flag=1;
     }
-    flag=0;//non lo è
+    return flag;
 }
-
 int main(){
-    string frase=NULL;
-    int len=0, pal;
-    char buffer[200];
-    
+    int dim=0, palindroma=0;
 
-    printf("inserisci una stringa: ");
-    fgets(buffer, sizeof (buffer), stdin);
+    String s = (String)malloc(50*sizeof(char));
+    if (s==NULL) return 1;
 
-    len=lenght(buffer);
-
-    compatta(buffer, len);
-    frase=(string)malloc((len+1)* sizeof (char));
-    if(frase==NULL){
-        printf("c'è un errore\n");
-        return 1;
-    }
-
-    strcpy(frase,buffer);
-    pal=palindroma(frase, len);
-
-    if(pal == 1) printf("la frase è palindroma ");
-    else printf("la frase non è palindroma ");
-    
-    free(frase);
-    return 0;
-    
+    printf("Inserisci una stringa: ");
+    fgets(s, 50, stdin);
+    printf("\n%s", s);
+    dim = lunghezza(s);
+    printf("\n%d", dim);
+    palindroma = Palindroma(s,dim);
+    if(palindroma==1)
+        printf("\nLa frase non è palindroma");
+    else    
+        printf("\nLa frase è palindroma");
 }
---
+++ 4-superiore/informatica/stringhe/libreriaStringheLibstring.c
@@ -0,0 +1,137 @@
+/*provare la libreria string.h per le seguenti operazioni
+1.calcolo lunghezza stringa
+2.controllo stringhe uguali
+3.copiare una stringa
+4.cercare un carattere e comunicare la frequenza
+5.concatenare due stringhe0
+6.ricerca di una strnga nell'altra
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef char * stringa;
+
+void compatta(stringa buffer, int len){
+    if(len>0 && buffer [len-1] == '\n'){
+        buffer[len-1]='\0';
+        len--;
+    }
+}
+/*1
+
+
+int main(){
+    stringa s=NULL;
+    char buffer[20];
+    int len=0;
+
+    printf("inserisci una stringa: ");
+    fgets(buffer,sizeof(buffer),stdin);
+
+    len=strlen(buffer);
+    compatta(buffer,len);
+    s=(stringa)malloc((len+1)*sizeof (char));
+    strcpy(s,buffer);
+
+    printf("la lunghezza della stringa è: %d",len);
+
+    free(s1);
+    exit(0);
+
+}*/
+
+/*2
+
+
+
+
+int main(){
+    stringa s1=NULL, s2=NULL;
+    int len1=0, len2=0;
+    char buffer1[20],buffer2[20];
+
+    printf("inserisci la prima stringa: ");
+    fgets(buffer1,sizeof(buffer1),stdin);
+
+    printf("inserisci la seconda stringa: ");
+    fgets(buffer2,sizeof(buffer2),stdin);
+
+    len1=strlen(buffer1);
+    len2=strlen(buffer2);
+
+    compatta(buffer1,len1);
+    compatta(buffer2,len2);
+
+    s1=(stringa)malloc((len1+1)*sizeof (char));
+    s2=(stringa)malloc((len2+1)*sizeof (char));
+
+    strcpy(s1,buffer1);
+    strcpy(s2,buffer2);
+    
+    if(strcmp(s1,s2)==0) printf("le stringhe sono uguali\n");
+    else if (strcmp(s1,s2)<0) printf("le stringhe sono diverse\n");
+    else if (strcmp(s1,s2)>0) printf("le stringhe sono diverse\n");
+
+    free(s1);
+    free(s2);
+    exit(0);
+    
+}*/
+
+/*3
+
+
+int main(){
+    stringa s1=NULL, s2=NULL;
+    int len1=0, len2=0;
+    char buffer1[20],buffer2[20];
+
+    printf("inserisci la prima stringa: ");
+    fgets(buffer1,sizeof(buffer1),stdin);
+
+
+    len1=strlen(buffer1);
+    len2=len1;
+
+    compatta(buffer1,len1);
+
+    s1=(stringa)malloc((len1+1)*sizeof (char));
+    s2=(stringa)malloc((len2+1)*sizeof (char));
+
+    strcpy(s1,buffer1);
+
+    //copia stringa
+    strcpy(s2,s1);
+    printf("stringa 1: %s\n",s1);
+    printf("stringa 2: %s",s2);
+
+    free(s1);
+    free(s2);
+    exit(0);
+}*/
+
+/*4
+
+*/
+int main(){
+    stringa s=NULL, p;
+    char buffer[20], carattere;
+    int len=0;
+
+    printf("inserisci una stringa: ");
+    fgets(buffer,sizeof(buffer),stdin);
+
+    len=strlen(buffer);
+    compatta(buffer,len);
+    s=(stringa)malloc((len+1)*sizeof (char));
+    strcpy(s,buffer);
+
+    printf("inserisci il carattere che vuoi cercare: ");
+    scanf("%c",&carattere);
+
+    p=strchr(s, carattere);
+    printf("la prima occorrenza trovata è: %s",p);
+
+}
